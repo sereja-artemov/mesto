5b118e1bd1dffa3945ed86936934112feca434f7
sereja-art <226@tweld.local>
2022-04-29 17:16:59 +0300

    Доделал валидацию

--- js/FormValidator.js
+++ js/FormValidator.js
@@ -1,5 +1,110 @@
+
 export class FormValidator {
-  constructor() {
-    
+  constructor(config, formElement) {
+    this._config = config;
+    this._formSelector = config.formSelector;
+    this._inputSelector = config.inputSelector;
+    this._submitButtonSelector = config.submitButtonSelector;
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._errorClass = config.errorClass;
+    this._formElement = formElement;
   }
+  
+  enableValidation() {
+
+    this._formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      _setEventListeners();
+    });
+    // Находим все поля внутри формы,
+    // сделаем из них массив методом Array.from
+    const inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
+    const buttonElement = this._formElement.querySelector(this._submitButtonSelector);
+  
+    this._toggleButtonState(inputList, buttonElement);
+    // Обойдём все элементы полученной коллекции
+    inputList.forEach((inputElement) => {
+      // каждому полю добавим обработчик события input
+      inputElement.addEventListener('input', () => {
+        // Внутри колбэка вызовем isValid,
+        // передав ей форму и проверяемый элемент
+        this._isValid(this._formElement, inputElement);
+        // Вызовем toggleButtonState и передадим ей массив полей и кнопку
+        this._toggleButtonState(inputList, buttonElement);
+      });
+    });
+  };
+
+  _toggleButtonState(inputList, buttonElement) {
+    // Если есть хотя бы один невалидный инпут
+    if (this._hasInvalidInput(inputList)) {
+      // сделай кнопку неактивной
+      this._disableFormButton(buttonElement, this._inactiveButtonClass);
+    } else {
+      // иначе сделай кнопку активной
+      buttonElement.classList.remove(this._inactiveButtonClass);
+      buttonElement.removeAttribute('disabled', 'disabled');
+    }
+  };
+// функция делает кнопку неактивной
+_disableFormButton(buttonElement, selector) {
+  buttonElement.classList.add(selector);
+  buttonElement.setAttribute('disabled', 'disabled');
 }
+  // Функция принимает массив полей
+  _hasInvalidInput(inputList) {
+    // проходим по этому массиву методом some
+    return inputList.some((inputElement) => {
+      // Если поле не валидно, колбэк вернёт true
+      // Обход массива прекратится и вся функция
+      // hasInvalidInput вернёт true
+
+      return !inputElement.validity.valid;
+    })
+  };
+
+  // Функция, которая проверяет валидность поля
+  _isValid(formElement, inputElement, rest) {
+    if (!inputElement.validity.valid) {
+      // Если поле не проходит валидацию, покажем ошибку
+      this._showInputError(formElement, inputElement, inputElement.validationMessage, rest);
+    } else {
+      // Если проходит, скроем
+      this._hideInputError(formElement, inputElement, rest);
+    }
+  };
+
+    // Функция, которая добавляет класс с ошибкой
+  _showInputError(formElement, inputElement, errorMessage) {
+    const formError = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(this._inputErrorClass);
+    formError.classList.add(this._errorClass);
+    formError.textContent = errorMessage;
+  };
+
+  // Функция, которая удаляет класс с ошибкой
+  _hideInputError(formElement, inputElement) {
+    const formError = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(this._inputErrorClass);
+    formError.classList.remove(this._errorClass);
+    formError.textContent = '';
+  };
+
+}
+
+const validationConfig = {
+  formSelector: '.form',
+  inputSelector: '.form__item',
+  submitButtonSelector: '.form__btn',
+  inactiveButtonClass: 'form__btn_status_disabled',
+  inputErrorClass: 'form__item_status_error',
+  errorClass: 'form__error-msg_active',
+}
+
+
+const formList = Array.from(document.querySelectorAll(validationConfig.formSelector));
+
+formList.forEach((formElement) => {
+  new FormValidator(validationConfig, formElement).enableValidation();
+});
